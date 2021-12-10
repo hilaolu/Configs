a0422e22a6456c04015d5d426244501d8ed1aa79
hilaolu <i@phyzait.moe>
2021-12-10 20:48:10 +0800

    update

---
+++ fish/fish_alias.fish
@@ -0,0 +1,30 @@
+alias gt="git tag"
+alias gp="git push"
+
+alias hi="himalaya"
+alias hiw="himalaya write"
+alias hid="himalaya delete"
+alias him="himalaya move"
+alias hir="himalaya read"
+alias hire="himalaya reply"
+alias hif="himalaya forward"
+
+alias vi="nvim"
+
+alias sr="sudo systemctl restart"
+alias ss="sudo systemctl start"
+alias st="sudo systemctl status"
+alias sdr="sudo systemctl daemon-reload"
+
+
+alias pa="podman attach"
+alias pal="podman attach --latest"
+alias pd="podman rm"
+alias pdl="podman rm --latest"
+alias pdf="podman rm -f"
+alias pdfl="podman rm -f --latest"
+alias ps="podman start"
+alias ps="podman start --latest"
+alias pr="podman restart"
+alias prl="podman restart --latest"
+alias pr="podman run"
